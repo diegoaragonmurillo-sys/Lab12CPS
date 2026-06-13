ca4530661b25198d4eea0cc587a12758d3cfbbe3
diegoaragonmurillo-sys <diego.aragon@tecsup.edu.pe>
2026-06-13 11:34:47 -0500

    fix: corregir aserción en MinishopApplicationTests

--- src/test/java/com/tecsup/minishop/MinishopApplicationTests.java
+++ src/test/java/com/tecsup/minishop/MinishopApplicationTests.java
@@ -2,18 +2,12 @@ package com.tecsup.minishop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class MinishopApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void errorDeliberado() {
-		assertEquals(99999, 1);
 	}
 
 }
