4d83cb4c4918c4d2bac18f50c78a2efa942ee5d3
diegoaragonmurillo-sys <diego.aragon@tecsup.edu.pe>
2026-06-13 11:26:20 -0500

    test: error deliberado para probar el pipeline

--- src/test/java/com/tecsup/minishop/MinishopApplicationTests.java
+++ src/test/java/com/tecsup/minishop/MinishopApplicationTests.java
@@ -2,12 +2,18 @@ package com.tecsup.minishop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class MinishopApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void errorDeliberado() {
+		assertEquals(99999, 1);
 	}
 
 }
